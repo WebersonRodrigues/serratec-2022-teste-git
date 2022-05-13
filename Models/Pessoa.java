83bb89d16d73d46b591bfb0cdc70da9f93361a80
Weberson Rodrigues <weberson.r.a@gmail.com>
2022-05-13 17:53:33 -0300

    :sparkles: Criando os geters e Seters

--- Models/Pessoa.java
+++ Models/Pessoa.java
@@ -12,5 +12,47 @@ public abstract class Pessoa {
     private Boolean desenvolvedor;
 
     private int idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Boolean getDesenvolvedor() {
+        return desenvolvedor;
+    }
+
+    public void setDesenvolvedor(Boolean desenvolvedor) {
+        this.desenvolvedor = desenvolvedor;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    
 }
 
