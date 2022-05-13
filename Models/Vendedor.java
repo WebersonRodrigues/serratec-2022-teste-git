8f18bf6993fe64bd355575f3c54c367956422e29
rodrigomramos21 <51106345+rodrigomramos21@users.noreply.github.com>
2022-05-13 18:26:09 -0300

    rodrigo

--- Models/Vendedor.java
+++ Models/Vendedor.java
@@ -3,5 +3,6 @@ package Models;
 public class Vendedor  extends Pessoa {
     private int cnpj;
     private double comicao;   
-    private String produtos; 
+    private String produtos;
+    private int jaja; 
 }
